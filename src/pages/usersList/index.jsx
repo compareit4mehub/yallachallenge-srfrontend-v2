9481b2db5d212853860973b28ab9fdf6e44b1968
mdazlaanzubair <mdazlaan1996@gmail.com>
2024-05-22 03:12:09 +0500

    minor issue fixed, title-bar icon changed

--- src/pages/usersList/index.jsx
+++ src/pages/usersList/index.jsx
@@ -131,7 +131,7 @@ export const UsersList = () => {
       ) : (
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-5">
           {filteredUsers?.length > 0 ? (
-            filteredUsers.map((item, index) => (
+            filteredUsers?.map((item, index) => (
               <UserCard key={index} user={item} />
             ))
           ) : (
